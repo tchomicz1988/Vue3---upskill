1d42096945db932134c0a6d0f68135dbdb6ce4c5
tchomicz <tchomicz@pgs-soft.com>
2021-12-14 13:46:21 +0100

    Sprint 3 fix

--- src/utils/form.utils.ts
+++ src/utils/form.utils.ts
@@ -14,33 +14,3 @@ export function enumToOptions(enumToTransform: any, keyAsValue = false): SelectO
 
   return options;
 }
-
-export const FormValidatorConst = {
-  isRequired: (value: string | number): boolean => {
-    return !!value?.toString()?.length;
-  },
-  maxLength: (value: string, maxLength: number): boolean => {
-    return value?.length > maxLength
-  },
-  minLength: (value: string, minLength: number): boolean => {
-    return value?.length < minLength
-  }
-}
-
-export class FormValidator {
-  static isRequired(value: string | number): boolean {
-    return !!value?.toString()?.length;
-  }
-
-  static maxLength(value: string, maxLength: number): boolean {
-    return value?.length > maxLength
-  }
-
-  static minLength(value: string, minLength: number): boolean {
-    return value?.length < minLength
-  }
-
-  static checkValidity() {
-
-  }
-}
